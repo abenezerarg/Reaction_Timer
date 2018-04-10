d66cb24ec6f43987059695f24fcb12c84ca5354a
abenezerarg <abenezerargaw@gmail.com>
2018-04-10 00:04:42 -0600

    Made Clock Divider Code and BCD_counter code

--- BCD_counter.v
+++ BCD_counter.v
@@ -1,3 +1,36 @@
-module BCD_counter ();
+module BCD_counter (Clock, Clear, E, BCD3, BCD2, BCD1, BCD0);
+input Clock, Clear, E;
+output reg [3:0]BCD3, BCD2, BCD1, BCD0;
+
+always @ ( posedge Clock ) begin
+    if (Clear) begin
+      BCD3 <= 0;
+      BCD2 <= 0;
+      BCD1 <= 0;
+      BCD0 <= 0;
+    end
+    else if (E) begin
+      if (BCD0 == 4'b1001) begin
+        BCD0 <= 0;
+        if(BCD1 == 4'b1001) begin
+          BCD1 <= 0;
+          if (BCD2 == 4'b1001) begin
+            BCD2 <= 0;
+            if (BCD3 == 4'b1001) begin
+              BCD3 <= 0;
+            end
+            else
+              BCD3 <= BCD3 + 1;
+          end
+          else
+            BCD2 <= BCD2 + 1;
+        end
+        else
+          BCD1 <= BCD1 + 1;
+      end
+      else
+        BCD3 <= BCD3 + 1;
+    end
+end
 
 endmodule //BCD_counter
--- Clock_divider.v
+++ Clock_divider.v
@@ -1,3 +1,15 @@
-module Clock_divider ();
+module Clock_divider (input clk, output reg clkout);
+reg [16:0]counter;
 
+initial begin
+  counter = 0;
+end
+
+always @ (posedge clk) begin
+  counter = counter + 1;
+  if (counter == 50000)begin
+    clkout = ~clkout;
+    counter = 0;
+  end
+end
 endmodule //Clock_divider
