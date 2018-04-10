283ec0f9912110af71964f4b9cbbf247b33832a4
abenezerarg <abenezerargaw@gmail.com>
2018-04-10 00:37:31 -0600

    Made Clock Divider Code and BCD_counter code

--- top_module.v
+++ top_module.v
@@ -1,3 +1,7 @@
-module top_module ();
+module top_module (MAX10_CLK1_50, KEY, HEX0, HEX1, HEX2, HEX3,LEDR);
+input [1:0] KEY;
+input MAX10_CLK1_50;
+output [6:0] HEX0, HEX1, HEX2, HEX3;
+output [9:0] LEDR;
 
 endmodule //
